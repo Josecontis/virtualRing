2c5e936e2412be7c7a9c1ad8fcd357b9a9e52070
Josecontis <peppeco98@gmail.com>
2023-11-04 18:49:45 +0100

    Fix env lights

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,11 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
 import { HeartRing } from "./HeartRing";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <pointLight position={[10, 10, 10]} />
-      <ambientLight intensity={0.5} />
-      <directionalLight color="white" position={[0, 0, 5]} intensity={1} />
+      <Environment preset="city" />
       <HeartRing />
     </>
   );
